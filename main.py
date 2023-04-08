dd62a01b5df2042a2e92a75da36881d434058d68
neeraavi <neeraavi@gmail.com>
2023-04-08 08:25:24 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,149 @@
+from operator import itemgetter
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+import sys
+from divui import Ui_MainWindow
+import transactions
+from datetime import datetime
+from TableModel import TableModel
+from CalendarModel import CalendarModel
+from CalendarDetailsModel import CalendarDetailsModel
+
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        self.now = datetime.now()
+        self.startYear = 2015
+        self.totalInvested = 0
+        self.transactions = None
+        self.transactionMap = {}
+        self.transactionsProxyModel = None
+        self.transactionsSourceModel = None
+        self.pathPrefix = '/home/iyerns/tmp/PycharmProjects/pythonProject/tmp'
+        self.sourceModel = None
+        self.proxyModel = None
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.btn_1.setText("ff")
+        self.ui.btn_1.clicked.connect(self.close)
+        self.fill_summary_table()
+        self.shortcut = QtWidgets.QShortcut(QtGui.QKeySequence(QtGui.QKeySequence.StandardKey.Find), self)
+        self.shortcut.activated.connect(self.ui.mainFilter.setFocus)
+        self.closeShortcut = QtWidgets.QShortcut(QtGui.QKeySequence(QtGui.QKeySequence.StandardKey.Quit), self)
+        self.closeShortcut.activated.connect(self.close)
+        self.closeShortcut = QtWidgets.QShortcut(QtGui.QKeySequence(QtGui.QKeySequence.StandardKey.Cancel), self)
+        self.closeShortcut.activated.connect(self.reset_main_filter)
+        self.ui.summaryView.setStyleSheet("alternate-background-color: #fefae0; background-color: white;")
+
+    def fill_summary_table(self):
+        t = transactions.Transactions(self.startYear)
+        t.fill_transactions(self.pathPrefix)
+
+        self.update_transaction_calendar(t)
+
+        (self.transactions, self.transactionMap, self.totalInvested, summaryHeader) = t.get_transaction_results()
+        self.sourceModel = TableModel(self.transactions, summaryHeader)
+        self.proxyModel = QtCore.QSortFilterProxyModel(self)
+        self.proxyModel.setFilterKeyColumn(-1)  # Search all columns.
+        self.proxyModel.setSourceModel(self.sourceModel)
+        self.proxyModel.sort(0, Qt.AscendingOrder)
+        self.proxyModel.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        self.ui.mainFilter.textChanged.connect(self.ticker_filter_changed)
+        self.ui.summaryView.setModel(self.proxyModel)
+        self.ui.summaryView.selectionModel().selectionChanged.connect(self.main_selection_changed)
+        self.ui.summaryView.resizeColumnsToContents()
+        self.ui.summaryView.show()
+
+    def update_transaction_calendar(self, t):
+        (self.investmentCalendar, investment_calendar_header, month_header) = t.get_investment_calendar()
+        self.investmentCalendarModel = CalendarModel(self.investmentCalendar, investment_calendar_header, month_header)
+        ##self.ui.investmentCalendarView.setModel(self.investmentCalendarModel)
+        # self.ui.investmentCalendarView.setColumnWidth(1, 100)
+        # self.ui.investmentCalendarView.horizontalHeader().setMaximumWidth(40)
+        # self.ui.investmentCalendarView.horizontalHeader().setMinimumWidth(30)
+
+        proxyModel = QtCore.QSortFilterProxyModel(self)
+        proxyModel.setFilterKeyColumn(-1)  # Search all columns.
+        proxyModel.setSourceModel(self.investmentCalendarModel)
+        # proxyModel.sort(2, Qt.AscendingOrder)
+        self.ui.investmentCalendarView.setModel(proxyModel)
+        # self.ui.investmentCalendarView.selectionModel().selectionChanged.connect(self.main_selection_changed)
+        # self.ui.investmentCalendarView.show()
+        # self.ui.investmentCalendarView.setModel(self.proxyModel)
+        self.ui.investmentCalendarView.selectionModel().selectionChanged.connect(self.inv_cal_selection_changed)
+
+        self.ui.investmentCalendarView.horizontalHeader().setStretchLastSection(False)
+        # self.ui.investmentCalendarView.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.ui.investmentCalendarView.resizeColumnsToContents()
+        self.ui.investmentCalendarView.setStyleSheet("alternate-background-color: #fefae0; background-color: white;")
+        self.ui.investmentCalendarView.clicked.connect(self.investment_calendar_clicked)
+
+    def inv_cal_selection_changed(self, selected, deselected):
+        for idx in selected.indexes():
+            r = idx.row()
+            c = idx.column()
+            self.redisplay_investment(r + 1, self.now.year - c)
+
+    def investment_calendar_clicked(self, item):
+        m = 1 + item.row()
+        y = self.now.year - item.column()
+        self.redisplay_investment(m, y)
+
+    def redisplay_investment(self, m, y):
+        if m > 13:
+            return
+
+        ym = '{y}-{m:02d}'.format(m=m, y=y)
+        # print(ym)
+        result = [row for ticker, t_list in self.transactionMap.items() for row in t_list if ym in row[2]]
+        result = sorted(result, key=itemgetter(2))
+        # print(result)
+        header = ['Ticker', 'B/S', 'Date', '#', 'Cost', 'CPS', 'Sign', 'z']
+        self.calendarDetailsModel = CalendarDetailsModel(result, header, [])
+        self.ui.calendarDetailsMode.setText(f'Investments in {ym}')
+        self.ui.calendarDetailsView.setModel(self.calendarDetailsModel)
+        self.ui.calendarDetailsView.setColumnHidden(6, True)
+        self.ui.calendarDetailsView.resizeColumnsToContents()
+        self.ui.calendarDetailsView.show()
+
+    def reset_main_filter(self):
+        self.ui.mainFilter.setFocus()
+        self.ui.mainFilter.setText('')
+
+    def ticker_filter_changed(self, new_filter):
+        self.proxyModel.setFilterFixedString(new_filter)
+        self.display_filtered_transactions(new_filter)
+
+    def display_filtered_transactions(self, ticker):
+        if ticker not in self.transactionMap:
+            return
+        filtered = self.transactionMap[ticker]
+        # print(ticker, filtered)
+        self.transactionsSourceModel = TableModel(filtered,
+                                                  ['Ticker', 'B/S', 'Date', '#', 'Cost', 'CPS', 'Sign', 'z'])
+        self.ui.transactionsView.setModel(self.transactionsSourceModel)
+        self.ui.transactionsView.setColumnHidden(6, True)
+        self.ui.transactionsView.resizeColumnsToContents()
+        self.ui.transactionsView.scrollToBottom()
+        self.ui.transactionsView.show()
+
+    def main_selection_changed(self, selected, deselected):
+        if len(selected.indexes()) > 0:
+            ticker = selected.indexes()[0].data()
+            print("selected: ", ticker)
+            self.display_filtered_transactions(ticker)
+
+
+def create_app():
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    win.ui.mainFilter.setFocus()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    create_app()
---
+++ transactions.py
@@ -0,0 +1,143 @@
+from datetime import datetime
+import calendar
+
+
+def process_file(filename, line_processor, meta_processor):
+    with open(filename) as file:
+        for line in file:
+            line_processor(line.rstrip())
+    return meta_processor()
+
+
+class Transactions:
+    def __init__(self, start):
+        self.prefix = None
+        self.totalInvested = 0
+        self.transactions = []
+        self.transactionMap = {}
+        self.investmentCalendar = []
+        self.investmentCalendarMap = {}
+        self.startYear = start
+        self.currentMonth = None
+        self.currentYear = None
+        self.now = None
+        self.init_investment_calendar()
+
+    def init_investment_calendar(self):
+        self.now = datetime.now()
+        for y in range(self.startYear, self.now.year + 1):
+            for m in range(1, 13):
+                ym = '{y}-{m:02d}'.format(y=y, m=m)
+                self.investmentCalendarMap[ym] = 0
+
+        self.numOfYears = (self.now.year - self.startYear + 1)
+        self.investmentCalendar = [[0] * self.numOfYears for i in range(12)]
+        # for s in self.investmentCalendar:
+        #    print(*s)
+        # print(json.dumps(self.transactionsCalendarMap, indent=4))
+
+    def transactions_processor(self, line):
+        if line.startswith('#'):
+            return None
+        else:
+            (name, yy, mm, dd, operation, nos, cost) = line.replace(' ', '').split('-')
+            op_sign = 1 if "Buy" in operation else -1
+            nos = int(nos)
+            cost = float(cost)
+            cps = "{:.2f}".format(cost / nos) if nos > 0 else '0.00'
+            ym = yy + '-' + mm
+            item = [name, operation, ym + '-' + dd, nos, cost, cps, op_sign]
+            if name not in self.transactionMap:
+                self.transactionMap[name] = []
+            self.transactionMap[name].append(item)
+
+    def create_transactions_table_from_list(self):
+        # [name, operation, ym + ' - ' + dd, nos, cost, cps, op_sign]'
+        #  0      1          2              3      4    5    6
+        for ticker, t_list in self.transactionMap.items():
+            inv = sum(row[4] * row[6] for row in t_list)
+            nos = sum(row[3] * row[6] for row in t_list)
+            if nos > 0:
+                self.totalInvested = self.totalInvested + inv
+                for row in t_list:
+                    y, m, _ = row[2].split('-')
+                    ym = '{y}-{m:02d}'.format(y=y, m=int(m))
+                    cost = row[4]
+                    op_sign = row[6]
+                    self.investmentCalendarMap[ym] = self.investmentCalendarMap[ym] + op_sign * cost
+            self.transactions.append([ticker, nos, round(inv)])
+            cps = "{:.2f}".format(inv / nos) if nos != 0 else ' '
+            t_list.append(['Total', len(t_list), '', nos, inv, cps, 'xx'])
+        self.totalInvested = round(self.totalInvested)
+        for item in self.transactions:
+            ticker, nos, invested = item
+            if nos == 0:
+                status = '*'
+                alloc = '0.00 %'
+            else:
+                status = ' '
+                alloc = "{:.2f} %".format(invested / self.totalInvested * 100)
+            item.insert(1, status)
+            item.append(alloc)
+        print(self.totalInvested)
+        self.cleanup_transaction_calendar()
+        self.process_names()
+
+    def cleanup_transaction_calendar(self):
+        for y in range(self.startYear, self.now.year + 1):
+            for m in range(1, 13):
+                ym = '{y}-{m:02d}'.format(y=y, m=m)
+                self.investmentCalendarMap[ym] = round(self.investmentCalendarMap[ym])
+
+        for m in range(1, 13):
+            inner_list = self.investmentCalendar[m - 1]
+            for y in range(self.now.year, self.startYear - 1, -1):
+                ym = '{y}-{m:02d}'.format(y=y, m=m)
+                inner_list[self.startYear - y - 1] = self.investmentCalendarMap[ym]
+        total_row = [sum(i) for i in zip(*self.investmentCalendar)]
+        self.investmentCalendar.append(total_row)
+        # sigma_row_empty = [x * 0 for x in total_row]
+        sigma_row = [x * 0 for x in total_row]
+        # self.investmentCalendar.append(sigma_row_empty)
+        sigma_row[0] = sum(total_row)
+        sigma_row[2] = 'ϕ'
+        sigma_row[3] = round(sigma_row[0] / self.numOfYears)
+        print(sum(total_row))
+        self.investmentCalendar.append(sigma_row)
+        print(total_row)
+
+    def process_names(self):
+        f = self.prefix + '/names.txt'
+        with open(f) as file:
+            for line in file:
+                line = line.rstrip()
+                (ticker, name, sector) = line.split('-')
+                ticker = ticker.lower().strip()
+                name = name.strip().replace('_', ' ')
+                sector = sector.strip()
+                for t in self.transactions:
+                    if ticker == t[0]:
+                        t.extend([name, sector])
+
+    def fill_transactions(self, prefix):
+        self.prefix = prefix
+        f = self.prefix + '/akt.txt'
+        return process_file(f, self.transactions_processor, self.create_transactions_table_from_list)
+
+    def get_transaction_results(self):
+        summary_header = ['Ticker', '.', '#', 'Invested', 'Alloc', 'Name', 'Sector']
+        return self.transactions, self.transactionMap, self.totalInvested, summary_header
+
+    def get_investment_calendar(self):
+        investment_calendar_header = [x for x in range(self.now.year, self.startYear - 1, -1)]
+        # investment_calendar_header.insert(0, 'Month')
+        month_names = list(calendar.month_name)[1:13]
+        month_names.append('Total')
+        # month_names.append('')
+        month_names.append('Σ')
+        # m = 0
+        # for row in self.investmentCalendar:
+        #    row.insert(0, month_names[m])
+        #    m = m + 1
+        self.investmentCalendar = [[' ' if x == 0 else x for x in l] for l in self.investmentCalendar]
+        return self.investmentCalendar, investment_calendar_header, month_names
